aa412a2efd0be2e7190910fcd5ba8bde7a9307e1
shasmeenZahra <asimkk347gmail.com>
2024-11-01 19:09:44 +0500

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Portfolio from "@/components/portfolio";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Portfolio from "./components/portfolio";
 
 const Home = () => (
   <div>
